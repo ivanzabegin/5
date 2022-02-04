5b38896731458382145344987786af10196c6510
Ivan Zabegin <IZA@opra.nl>
2022-02-04 19:02:25 +0300

    ex.2 completed. works

---
+++ Seminars/2/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = 8;
+Console.WriteLine(numberA);
+int numberB = 8;
+Console.WriteLine(numberB);
+
+if (numberA > numberB)
+{
+    Console.WriteLine("max=" + numberA);
+    Console.WriteLine("min=" + numberB);
+}
+
+else if (numberA < numberB)
+{
+    Console.WriteLine("max=" + numberB);
+    Console.WriteLine("min=" + numberA);
+}
+
+else if (numberA==numberB)
+
+{
+    Console.WriteLine("No max or min, equally. " +  numberA+"="+numberB);
+}
