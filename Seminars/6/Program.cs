ebd2239a7a37b432c219c3670c3ac17674e456c9
Ivan Zabegin <IZA@opra.nl>
2022-02-04 20:02:17 +0300

    ex 6 works

---
+++ Seminars/6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+if (numberA%2==0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
