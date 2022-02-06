a673f75e0be4e0c1b8897ede2bdd0516c42d4342
Ivan Zabegin <IZA@opra.nl>
2022-02-07 00:37:43 +0300

    ex12 works

---
+++ Seminars/12/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+string s = a.ToString();
+string c = s.Remove (1, 1);
+Console.WriteLine(c);
