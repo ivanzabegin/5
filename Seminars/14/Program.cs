8f980894ba754a35da3814a2e550f8d977666228
Ivan Zabegin <IZA@opra.nl>
2022-02-12 18:10:40 +0300

    ex14 works

---
+++ Seminars/14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+int a = new Random().Next(1, 999);
+Console.WriteLine(a);
+char [] s = a.ToString().ToCharArray();
+//Console.WriteLine(s);
+//Console.WriteLine(s.Length);
+int b = a/100;
+//Console.WriteLine(b);
+if (b!=0) Console.WriteLine(s[2]);
+else Console.WriteLine("no 3rd number"); 
+
