aeeb4549c7704780642d22a3803c11d12bcf0c47
Ivan Zabegin <IZA@opra.nl>
2022-02-12 21:49:16 +0300

    2nd seminar completed

---
+++ Seminars/17/Program.cs
@@ -0,0 +1,8 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int numbera = 25;
+int numberb = 5;
+if (Math.Sqrt(numbera)==numberb) Console.WriteLine("1st is quadrat");
+else Console.WriteLine("no 1 st isn't quadrat");
+if (Math.Sqrt(numberb)==numbera) Console.WriteLine("2nd is quadrat");
+else Console.WriteLine("2ns isn't quadrat");
