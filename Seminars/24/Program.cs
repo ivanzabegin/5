278dca4ca81543710474c17019955428a3f01f01
Ivan Zabegin <IZA@opra.nl>
2022-02-13 22:57:51 +0300

    ex24 works

---
+++ Seminars/24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 24: Найти кубы чисел от 1 до N
+
+Console.WriteLine("Enter number");
+Console.ReadLine();
+int a = int.Parse(Console.ReadLine());
+int i=1;
+
+while (i < a+1);
+
+{
+    Console.WriteLine(i + "=" + i*i*i);
+    i++;
+}
