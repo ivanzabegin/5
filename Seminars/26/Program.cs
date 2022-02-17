19b22ebaeb0773e137987be4aa964252c9678e8f
Ivan Zabegin <IZA@opra.nl>
2022-02-17 19:36:56 +0300

    ex 26 works

---
+++ Seminars/26/Program.cs
@@ -0,0 +1,9 @@
+﻿// Возведите число А в натуральную степень B, используя цикл
+
+Console.Clear();
+Console.Write("введите число:"); 
+int a = int.Parse(Console.ReadLine());
+for (int power = 0; power <= 32; power++)
+   {
+       Console.WriteLine($"{a}^{power} = {(long)Math.Pow(a, power):N0}(0x{(long)Math.Pow(a, power):X})");
+   }
