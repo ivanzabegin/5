49bedb6146d1047e4769838cfc563318d9973448
Ivan Zabegin <IZA@opra.nl>
2022-02-22 22:18:36 +0300

    ex 28 works

---
+++ Seminars/28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитайте сумму цифр в числе.
+
+Console.Clear();
+Console.WriteLine("введи число: ");
+int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
+Console.WriteLine();
