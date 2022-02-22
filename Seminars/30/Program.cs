e791602b0e9a1ecef70a1105a1a2abb6e58daa4e
Ivan Zabegin <IZA@opra.nl>
2022-02-22 23:24:01 +0300

    ex30 works

---
+++ Seminars/30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+public class SamplesArray
+{
+    public static void Main()
+    {
+        
+        int[] intArray = new int[5];
+
+        for (int i = 0; i < intArray.Length; i++)
+    {
+        intArray [i] = new Random().Next(1, 100);
+    }
+
+        
+        Action<int> action = new Action<int>(ShowCubed);
+
+        Array.ForEach(intArray, action);
+    }
+
+    private static void ShowCubed(int val)
+    {
+        Console.WriteLine("{0:d} cubed = {1:d}", val, val*val*val);
+    }
+}
