6b42cda0f3f59b94face822b381728b345a60bf0
Ivan Zabegin <IZA@opra.nl>
2022-02-23 21:48:59 +0300

    ex30 works

--- Seminars/30/Program.cs
+++ Seminars/30/Program.cs
@@ -2,24 +2,29 @@
 
 public class SamplesArray
 {
-    public static void Main()
-    {
-        
-        int[] intArray = new int[5];
+static void PrintValues(int[] intArray)
+{
+foreach (int i in intArray)
+{
+Console.Write("\t{0}", i);
+}
+Console.WriteLine();
+}
 
-        for (int i = 0; i < intArray.Length; i++)
-    {
-        intArray [i] = new Random().Next(1, 100);
-    }
+public static void Main()
+{
+int[] intArray = new int[5];
+for (int i = 0; i < intArray.Length; i++)
+{
+intArray [i] = new Random().Next(1, 100);
+}
+PrintValues(intArray);
+Action<int> action = new Action<int>(ShowCubed);
+Array.ForEach(intArray, action);
 
-        
-        Action<int> action = new Action<int>(ShowCubed);
-
-        Array.ForEach(intArray, action);
-    }
-
-    private static void ShowCubed(int val)
-    {
-        Console.WriteLine("{0:d} cubed = {1:d}", val, val*val*val);
-    }
+}
+private static void ShowCubed(int val)
+{
+if (val %2 ==0) Console.WriteLine("{0:d} cubed = {1:d}", val, val*val*val);
+}
 }
