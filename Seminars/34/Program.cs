49423b52a2655102c6571813b04c675b1d75fc18
Ivan Zabegin <IZA@opra.nl>
2022-02-23 23:15:36 +0300

    ex34 works

---
+++ Seminars/34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу для замены элементов массива на противоположные.
+
+public class SamplesArray1  {
+
+   public static void Main()  
+   {
+      int[] intArray = new int[9] {1,2,3,4,5,6,7,8,9};
+      Console.WriteLine( "The Array initially contains the following values:" );
+      PrintIndexAndValues( intArray );
+
+      Array.Reverse( intArray);
+
+
+      Console.WriteLine( "After reversing:" );
+      PrintIndexAndValues( intArray );
+   }
+
+   public static void PrintIndexAndValues( Array intArray )  {
+      for ( int i = intArray.GetLowerBound(0); i <= intArray.GetUpperBound(0); i++ )
+         Console.WriteLine( "\t[{0}]:\t{1}", i, intArray.GetValue( i ) );
+   }
+}
