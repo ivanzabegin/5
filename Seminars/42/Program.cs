d8019ae1ae4450be81397af3225b9cf634e531a7
Ivan Zabegin <IZA@opra.nl>
2022-03-09 00:28:23 +0300

    ex42 works

---
+++ Seminars/42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+Console.Clear();
+
+Console.WriteLine("Введите 10 чисел:");
+int[] a = new int[10];
+int count = 0;
+for (int i = 0; i < a.Length; i++)
+{
+a[i] = Convert.ToInt32(Console.ReadLine());
+if (a[i] > 0) 
+count++;
+}
+Console.WriteLine("чисел больше 0:  " + count);
