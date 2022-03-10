c2d602131e5b4d0d1751f799c45989904d12f253
Ivan Zabegin <IZA@opra.nl>
2022-03-10 23:34:49 +0300

    ex44 works

---
+++ Seminars/44/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, если b1, k1, b2 и k2 заданы
+
+
+Console.Clear();
+
+Console.WriteLine("Введите коэффициент b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2)
+Console.WriteLine("lines parallel");
+if (k1 * k2 == (-1))
+Console.WriteLine("lines ortangular");
+double x = 0;
+double y =0;
+x = (b2-b1)/(k1-k2);
+y = k2 * x + b2;
+Console.WriteLine($"координата х={x}, координата y={y}");
