8e8e7c3f69badfe3cbe6719902b9812698ae488b
Ivan Zabegin <IZA@opra.nl>
2022-03-15 20:53:58 +0300

    ex57 works

---
+++ Seminars/57/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+internal class Program
+    {
+        private static void Main(string[] args)
+        {
+ 
+            var matrix = new int[5, 7];
+            var rnd = new Random();
+ 
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(0, 100);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+ 
+            var arr = matrix.Cast<int>().OrderByDescending(a => a).ToArray();
+ 
+            int c = 0;
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    matrix[j, k] = arr[c];
+                    Console.Write(matrix[j, k] + " ");
+                    c++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
