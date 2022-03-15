442bdf2d7f5f5d3135558cda89fc20b47b0f380b
Ivan Zabegin <IZA@opra.nl>
2022-03-15 21:41:56 +0300

    ex59 works

---
+++ Seminars/59/Program.cs
@@ -0,0 +1,62 @@
+﻿//  В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+Console.Clear();
+ 
+int n1, n2;
+            int[,] A;
+ 
+            Console.Write("Введите количество строк в матрице: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в матрице: ");
+            n2 = int.Parse(Console.ReadLine());
+            A = new int[n1, n2];
+            Random rnd = new Random();
+ 
+            // Присвоение значений датчиком случайных чисел
+            for (int i = 0; i < n1; i++)
+                for (int j = 0; j < n2; j++)
+                    A[i, j] = rnd.Next(-0, 10 + 1);
+ 
+            //Вывод массива
+            for (int i = 0; i < n1; i++, Console.WriteLine())
+                for (int j = 0; j < n2; j++)
+                    Console.Write(A[i, j] + "\t");
+int minRowSum = int.MaxValue, indexMinRow = 0;
+ 
+             for (int i = 0; i < n1; i++)
+             {
+                int rowSum = 0;
+                for (int j = 0; j < n2; j++)
+                    rowSum += A[i, j];
+                
+                if (rowSum < minRowSum)
+                {
+                    minRowSum = rowSum;
+                    indexMinRow = i;
+                }
+             }
+ 
+             Console.WriteLine("Строка с минимальной суммой элементов");
+                 for(int j = 0;j<n2;j++)
+                      Console.Write(A[indexMinRow, j] + "\t");
+Console.WriteLine();
+
+int maxRowSum = int.MinValue, indexMaxRow = 0;
+ 
+             for (int i = 0; i < n1; i++)
+             {
+                int rowSum = 0;
+                for (int j = 0; j < n2; j++)
+                    rowSum += A[i, j];
+                
+                if (rowSum > maxRowSum)
+                {
+                    maxRowSum = rowSum;
+                    indexMaxRow = i;
+                }
+             }
+ 
+             Console.WriteLine("Строка с максимальной суммой элементов");
+                 for(int j = 0;j<n2;j++)
+                      Console.Write(A[indexMaxRow, j] + "\t");
+Console.WriteLine();
