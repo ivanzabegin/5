2e48c2f137ed3647ad3547f423c6541f15858223
Ivan Zabegin <IZA@opra.nl>
2022-03-18 23:44:45 +0300

    ex69 works

---
+++ Seminars/69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму элементов в промежутке от M до N.
+ 
+static int RecursiveMethod(int m, int n)
+{
+    
+    if (m == n)
+    {
+        return m;
+    }
+    else 
+    {
+        return m + RecursiveMethod(m + 1, n);
+    }
+}
+
+Console.WriteLine(RecursiveMethod(0 ,4));
+   
