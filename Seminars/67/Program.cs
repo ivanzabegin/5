0a3e203c52d7b8c98b4457da1a1da2423e07948f
Ivan Zabegin <IZA@opra.nl>
2022-03-18 23:31:54 +0300

    ex67 works

---
+++ Seminars/67/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+
+class RecExercise1
+{
+    static int printNatural(int ctr,int stval)
+    {
+	if (ctr < 1)
+	{
+	    return stval;
+	}
+	Console.Write(" {0} ",ctr);
+		ctr--;
+	return printNatural(ctr,stval);
+    }
+    
+    static void Main()
+    {
+	
+	Console.Write("Задать N : ");
+	int ctr= Convert.ToInt32(Console.ReadLine());
+	printNatural(ctr,1);
+	Console.Write("\n\n");
+    }
+}
