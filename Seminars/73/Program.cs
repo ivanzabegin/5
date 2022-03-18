862473dffdb4accb4fac3d4f393edef6581d00b3
Ivan Zabegin <IZA@opra.nl>
2022-03-18 23:57:36 +0300

    ex73 works

---
+++ Seminars/73/Program.cs
@@ -0,0 +1,27 @@
+﻿// Есть число N. Сколько групп M, можно получить при разбиении всех чисел на группы, так чтобы в одной группе все числа были взаимно просты (все числа в группе друг на друга не делятся)? 
+// Найдите M при заданном N и получите одно из разбиений на группы N ≤ 10²⁰
+
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int numb = Int32.Parse(Console.ReadLine());
+            int groop = 0;
+            for(int i= 1; i<=numb; i++)
+            {
+                if(i ==Math.Pow(2,groop))
+                {
+                    Console.WriteLine();
+                    groop++;
+                    Console.Write($"{groop}-я группа");
+                }
+                Console.Write($"{i,5}") ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
